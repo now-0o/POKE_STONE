4289b709f571a1b56ba367420d3c06d18792160f
now-0o <117905287+now-0o@users.noreply.github.com>
2026-08-14 21:53:54 +0900

    refactor: 신오 전용 카드 데이터를 카드 모듈로 분리

---
+++ src/data/cards/sinnoh.js
@@ -0,0 +1,90 @@
+import {
+  TRAINER_CARDS,
+  CARD_MAP,
+  DEX,
+  ABILITY_TEXT,
+} from "../cards.js";
+
+const SINNOH_TRAINER_CARDS = [
+  {
+    id: "sinnoh_maylene_lucario",
+    name: "자두의 루카리오",
+    kind: "pokemon",
+    type: "격투",
+    cost: 5,
+    atk: 7,
+    hp: 8,
+    rarity: "L",
+    stage: 0,
+    ability: "maylene_aurasphere",
+    signature: true,
+    trainerOnly: true,
+  },
+  {
+    id: "sinnoh_wake_floatzel",
+    name: "맥실러의 플로젤",
+    kind: "pokemon",
+    type: "물",
+    cost: 5,
+    atk: 7,
+    hp: 7,
+    rarity: "L",
+    stage: 0,
+    ability: "wake_aquajet",
+    signature: true,
+    trainerOnly: true,
+  },
+  {
+    id: "sinnoh_fantina_mismagius",
+    name: "멜리사의 무우마직",
+    kind: "pokemon",
+    type: "고스트",
+    cost: 5,
+    atk: 6,
+    hp: 8,
+    rarity: "L",
+    stage: 0,
+    ability: "fantina_drain",
+    signature: true,
+    trainerOnly: true,
+  },
+  {
+    id: "sinnoh_byron_bastiodon",
+    name: "동관의 바리톱스",
+    kind: "pokemon",
+    type: "강철",
+    cost: 5,
+    atk: 4,
+    hp: 10,
+    rarity: "L",
+    stage: 0,
+    ability: "byron_ironwall",
+    secondaryAbility: "taunt",
+    signature: true,
+    trainerOnly: true,
+  },
+];
+
+for (const card of SINNOH_TRAINER_CARDS) {
+  if (CARD_MAP[card.id]) continue;
+  TRAINER_CARDS.push(card);
+  CARD_MAP[card.id] = card;
+}
+
+Object.assign(DEX, {
+  sinnoh_maylene_lucario: 448,
+  sinnoh_wake_floatzel: 419,
+  sinnoh_fantina_mismagius: 429,
+  sinnoh_byron_bastiodon: 411,
+});
+
+Object.assign(ABILITY_TEXT, {
+  maylene_aurasphere:
+    "파동탄: 즉시 공격. 이번 턴 자두가 이미 공격한 횟수만큼 이번 공격 피해가 증가한다. 최대 +2.",
+  wake_aquajet:
+    "아쿠아제트: 즉시 공격. 현재 수몰된 플레이어 필드칸 하나당 이번 공격 피해 +1.",
+  fantina_drain:
+    "흡수: 플레이어의 유령이 자연 소멸할 때 그 기운을 흡수해 +1/+1을 얻는다. 최대 3회.",
+  byron_ironwall:
+    "철벽: 방어도 2로 등장한다. 자신의 턴 종료 시 방어도 +1(최대 2). 방어도가 공격으로 완전히 파괴될 때마다 메탈버스트로 공격자에게 피해 1.",
+});
