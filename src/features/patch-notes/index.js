f19b38a8142a610314c4856b43fff1bf5057cff9
now-0o <117905287+now-0o@users.noreply.github.com>
2026-08-17 22:46:17 +0900

    Show v2.8 patch notes

--- src/features/patch-notes/index.js
+++ src/features/patch-notes/index.js
@@ -1,4 +1,5 @@
 import { PATCH_NOTES } from "../../patchNotes.js";
 import { PATCH_NOTE_V27 } from "./v27.js";
+import { PATCH_NOTE_V28 } from "./v28.js";
 
-export const ALL_PATCH_NOTES = [PATCH_NOTE_V27, ...PATCH_NOTES];
+export const ALL_PATCH_NOTES = [PATCH_NOTE_V28, PATCH_NOTE_V27, ...PATCH_NOTES];
