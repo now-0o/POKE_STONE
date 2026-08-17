08e282aeac9217bb3a1dac7fec78fe3f0eb14e70
now-0o <117905287+now-0o@users.noreply.github.com>
2026-08-17 13:06:24 +0900

    Rename Wallace Milotic ability display

--- src/data/cards/labels.js
+++ src/data/cards/labels.js
@@ -1,6 +1,11 @@
-import { CARD_MAP } from "../../data/cards.js";
+import { ABILITY_TEXT, CARD_MAP } from "../../data/cards.js";
 
 // 기존 카드 ID/효과는 유지하고 화면에 표시되는 이름만 정리한다.
 if (CARD_MAP.recover) {
   CARD_MAP.recover.name = "HP회복";
+}
+
+if (ABILITY_TEXT.primordialsea) {
+  ABILITY_TEXT.primordialsea =
+    "하이드로펌프: 나왔을 때 비를 내리고, 상대 포켓몬 전체에게 물 타입 피해 2";
 }
