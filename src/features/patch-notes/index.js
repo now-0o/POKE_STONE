192c022a3d5d6b991ff0c0265efd602959d6ea72
now-0o <117905287+now-0o@users.noreply.github.com>
2026-08-21 15:05:20 +0900

    feat: register 3.2 patch notes

--- src/features/patch-notes/index.js
+++ src/features/patch-notes/index.js
@@ -3,8 +3,10 @@ import { PATCH_NOTE_V27 } from "./v27.js";
 import { PATCH_NOTE_V28 } from "./v28.js";
 import { PATCH_NOTE_V30 } from "./v30.js";
 import { PATCH_NOTE_V31 } from "./v31.js";
+import { PATCH_NOTE_V32 } from "./v32.js";
 
 export const ALL_PATCH_NOTES = [
+  PATCH_NOTE_V32,
   PATCH_NOTE_V31,
   PATCH_NOTE_V30,
   PATCH_NOTE_V28,
