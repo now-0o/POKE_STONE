058d101211b236ebccdde3bdb14b4668db5dfd3b
now-0o <117905287+now-0o@users.noreply.github.com>
2026-08-24 17:26:24 +0900

    register v3.3 patch notes

--- src/features/patch-notes/index.js
+++ src/features/patch-notes/index.js
@@ -4,8 +4,10 @@ import { PATCH_NOTE_V28 } from "./v28.js";
 import { PATCH_NOTE_V30 } from "./v30.js";
 import { PATCH_NOTE_V31 } from "./v31.js";
 import { PATCH_NOTE_V32 } from "./v32.js";
+import { PATCH_NOTE_V33 } from "./v33.js";
 
 export const ALL_PATCH_NOTES = [
+  PATCH_NOTE_V33,
   PATCH_NOTE_V32,
   PATCH_NOTE_V31,
   PATCH_NOTE_V30,
