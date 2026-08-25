0ee827dcf9d3a657f0e8e66fa5976633d60cc972
now-0o <117905287+now-0o@users.noreply.github.com>
2026-08-25 22:03:29 +0900

    Show v3.5 patch notes

--- src/features/patch-notes/index.js
+++ src/features/patch-notes/index.js
@@ -5,8 +5,10 @@ import { PATCH_NOTE_V30 } from "./v30.js";
 import { PATCH_NOTE_V31 } from "./v31.js";
 import { PATCH_NOTE_V32 } from "./v32.js";
 import { PATCH_NOTE_V33 } from "./v33.js";
+import { PATCH_NOTE_V35 } from "./v35.js";
 
 export const ALL_PATCH_NOTES = [
+  PATCH_NOTE_V35,
   PATCH_NOTE_V33,
   PATCH_NOTE_V32,
   PATCH_NOTE_V31,
